2ff81de83a369a7d0d890bee8b4a3641cf07d5f4
Teresa Heidt <teresaheidt@hotmail.com>
2020-10-23 09:37:12 -0500

    updated models

---
+++ models/workout.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+    {
+  day: {
+    type: date,
+    default: date.now
+  },
+
+  exercise: [
+      {
+        type: {
+            type: String,
+            trim: true,
+            required: 'Enter excercise type'
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: 'Enter exercise name'
+        },
+        duration: {
+            type: Number,
+            required: 'Enter duration of the exercise in minutes'
+        },
+        weight: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+        },
+        sets: {
+            type: Number,
+        },
+        distance: {
+            type: Number
+        }
+    }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true
+    }
+}
+);
+
+workoutSchema.virtual('totalDuration').get(function() {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const workout = mongoose.model("workout", workoutSchema);
+
+module.exports = workout;
